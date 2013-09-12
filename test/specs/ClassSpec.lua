d93ef2ae11bdfe28ac296602ab95b0133f2f3e39
Spence DiNicolantonio <spence.dinicolantonio@gmail.com>
2013-09-12 06:36:22 -0400

    Added tests to the class spec

--- test/specs/ClassSpec.lua
+++ test/specs/ClassSpec.lua
@@ -2,59 +2,103 @@ require("LuaOO")
 
 describe("Class", function()
 
-	it("can be instantiated", function()
-		local object = Object:New();
-		assert.truthy(object)
+	local MyClass;
+
+	before_each(function()
+		MyClass = Object:Extend("MyClass");
 	end)
 
-	it("can be extended", function()
-		local class = Object:Extend("MyClass");
-		assert.truthy(class);
+
+	it("can be instantiated", function()
+		local instance = MyClass:New();
+		assert.truthy(instance);
 	end)
+
+
+	it("can be extended", function()
+		local subclass = MyClass:Extend("MySubclass");
+		assert.truthy(subclass);
+	end)
+
 
 	it("cannot be extended without class name", function()
 		assert.has.error(function()
-			Object:Extend();
+			MyClass:Extend();
 		end)
 		assert.has.error(function()
-			Object:Extend(2);
+			MyClass:Extend(2);
 		end)
 		assert.has.error(function()
-			Object:Extend(function() end);
+			MyClass:Extend(function() end);
 		end)
 	end)
+
 
 	it("can report its name", function()
-		local MyClass = Object:Extend("MyClass");
-		assert.equals(Object:GetName(), "Object")
-		assert.equals(MyClass:GetName(), "MyClass")
+		local MySubclass = Object:Extend("MySubclass");
+		assert.equals(MyClass:GetName(), "MyClass");
+		assert.equals(MySubclass:GetName(), "MySubclass");
 	end)
 
+
 	it("can report its superclass", function()
-		local MyClass = Object:Extend("MyClass");
 		local MySubclass = MyClass:Extend("MySubclass");
-		assert.Nil(Object:Parent());
 		assert.same(MyClass:Parent(), Object);
 		assert.same(MySubclass:Parent(), MyClass);
 	end)
 
+
 	it("can dertermine if it extends a class", function()
-		local MyClass = Object:Extend("MyClass");
 		local MySubclass = MyClass:Extend("MySubclass");
 		assert.True(MyClass:Extends(Object));
 		assert.True(MySubclass:Extends(MyClass));
+		assert.True(MySubclass:Extends(Object));
 		assert.False(MyClass:Extends(MySubclass));
-		assert.False(Object:Extends(nil));
 	end)
 
+
 	it("can determine if an object is an instance of itself", function()
-		local MyClass = Object:Extend("MyClass");
 		local MyOtherClass = Object:Extend("MyOtherClass");
-		local myInstance = MyClass:New();
-		assert.True(MyClass:IsInstance(myInstance));
-		assert.True(Object:IsInstance(myInstance));
-		assert.False(MyOtherClass:IsInstance(myInstance));
+		local instance = MyClass:New();
+		assert.True(MyClass:IsInstance(instance));
+		assert.True(Object:IsInstance(instance));
+		assert.False(MyOtherClass:IsInstance(instance));
 		assert.False(MyOtherClass:IsInstance(nil));
+	end)
+
+
+	it("cannot have public fields", function()
+		assert.has.error(function()
+			MyClass.someField = "value";
+		end);
+		assert.has.error(function()
+			MyClass.final.someField = "value";
+		end);
+	end)
+
+
+	it("can have public methods", function()
+		function MyClass:SomeMethod() end
+	end)
+
+
+	it("can have static fields", function()
+		Object.static.someField = "value";
+	end)
+
+
+	it("can have static methods", function()
+		function Object.static:SomeStaticMethod() end
+	end)
+
+
+	it("can have public final methods", function()
+		function Object.final:SomeFinalMethod() end
+	end)
+
+
+	it("can have static final methods", function()
+		function Object.static.final:SomeStaticFinalMethod() end
 	end)
 
 end)
